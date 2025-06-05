128cbfaebfe18f72ce91db03a4e24afed68faaf8
Herve Desrosiers <hdesrosiers@stingray.com>
2025-06-05 09:13:54 -0400

    onKeyDown channel card prop

--- src/components/ChannelCard/ChannelCard.tsx
+++ src/components/ChannelCard/ChannelCard.tsx
@@ -74,6 +74,7 @@ export const ChannelCard = React.forwardRef<HTMLDivElement, ChannelCardProps>(
           onClick={onClick}
           onFocus={onFocus}
           onBlur={onBlur}
+          onKeyDown={onKeyDown}
           {...rest}
         >
           {thumbnailUrl ? (
